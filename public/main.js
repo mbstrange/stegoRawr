f8c331de00d5e824906913f3ac0de62c1e1a6ea7
sjfricke <sjfricke@wisc.edu>
2017-01-21 07:40:10 -0600

    Audio Works
    need SSL certificate now though

--- public/main.js
+++ public/main.js
@@ -1,3 +1,58 @@
-function getSong() {
+(function(window) {
+  var client = new BinaryClient('ws://localhost:9001', "test");
     
-}
+  client.on('open', function() {
+    window.Stream = client.createStream();
+
+    if (!navigator.getUserMedia)
+      navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+    if (navigator.getUserMedia) {
+      navigator.getUserMedia({audio:true}, success, function(e) {
+        alert('Error capturing audio.');
+      });
+    } else alert('getUserMedia not supported in this browser.');
+
+    var recording = false;
+
+    window.getSong = function() {
+      recording = true;
+      setTimeout(function(){
+          recording = false;
+          window.Stream.end()
+      }, 3000); // 3 seconds of recording
+    }
+
+    function success(e) {
+      audioContext = window.AudioContext || window.webkitAudioContext;
+      context = new audioContext();
+
+      // the sample rate is in context.sampleRate
+      audioInput = context.createMediaStreamSource(e);
+
+      var bufferSize = 2048;
+      recorder = context.createScriptProcessor(bufferSize, 1, 1);
+
+      recorder.onaudioprocess = function(e){
+        if(!recording) return;
+        console.log ('recording');
+        var left = e.inputBuffer.getChannelData(0);
+        window.Stream.write(convertoFloat32ToInt16(left));
+      }
+
+      audioInput.connect(recorder)
+      recorder.connect(context.destination); 
+    }
+
+    function convertoFloat32ToInt16(buffer) {
+      var l = buffer.length;
+      var buf = new Int16Array(l)
+
+      while (l--) {
+        buf[l] = buffer[l]*0xFFFF;    //convert to 16 bit
+      }
+      return buf.buffer
+    }
+  });
+})(this);
--- server.js
+++ server.js
@@ -1,35 +1,20 @@
 //-------------------------Module "Importing"-----------------------------//
-var express = require('express'); //used as routing framework
-var app = express(); //creates an instance of express
-
-//modules required (same idea of #includes or Imports)
+var express = require('express');
+var BinaryServer = require('binaryjs').BinaryServer;
+var fs = require('fs');
+var wav = require('wav');
 var path = require('path'); //Node.js module used for getting path of file
 var logger = require('morgan'); //used to log in console window all request
-var cookieParser = require('cookie-parser'); //Parse Cookie header and populate req.cookies
 var bodyParser = require('body-parser'); //allows the use of req.body in POST request
-var server = require('http').createServer(app); //creates an HTTP server instance
-var extend = require('util')._extend; //used to make copy of objects -> extend({}, objToCopy);
+
+var app = express(); //creates an instance of express
+
 
 //-------------------------Globals-----------------------------//
 var localIP = require("ip").address(); //used to know where to check for web view site
 console.log("Local IP: " + localIP);
 
 var debugMode = false;
-//-------------------------getting funtions/routes from other files-----------------------------//
-//api to mongoose calls
-var api = require('./routes/api');
-
-//-------------------------Sets up MongoDB Connection-----------------------------//
-/*var mongoURI = "mongodb://127.0.0.1:27017/EggPlant"; //localhost:defaultPort/dataBase
-
-//sets up Mongoose
-var mongoose = require('mongoose'); //added for Mongo support
-var MongooseDB = mongoose.connect(mongoURI).connection; //makes connection
-MongooseDB.on('error', function(err) { console.log(err.message); console.log("Is MongoDB Running?"); });
-MongooseDB.once('open', function() {
-  console.log("mongooseDB connection open");
-});
-      */
 //-------------------------Node Setup-----------------------------//
 //Loops through starting after "node server.js" and checks the arguments
 for (var i = 2; i < process.argv.length; i++) {
@@ -47,12 +32,10 @@ for (var i = 2; i < process.argv.length; i++) {
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public'))); //sets all static file calls to folder
 
 
 //-------------------------ROUTES-----------------------------//
-app.use('/api', api); //sets the API used to access the Database
 
 app.get('/', function(req, res, next) {
    res.sendFile(path.join(__dirname, '../public', 'index.html'));
@@ -65,8 +48,8 @@ app.get('/song/:id', function(req, res, next) {
 
 
 //-------------------------HTTP Server Config-----------------------------//
-server.listen(8451); //Port to listen on
-server.on('listening', onListening);
+app.listen(8451); //Port to listen on
+app.on('listening', onListening);
 
 function onListening() {
   var addr = server.address();
@@ -76,5 +59,30 @@ function onListening() {
   console.log('Listening on ' + bind);
 }
 
+//-------------------------Binary Audio Server-----------------------------//
+binaryServer = BinaryServer({port: 9001});
+
+binaryServer.on('connection', function(client) {
+    
+  console.log('new connection from: ' + client.id);
+  
+  var outFile = "audio/" + "sound_" + client.id + ".wav"; 
+    
+  var fileWriter = new wav.FileWriter(outFile, {
+    channels: 1,
+    sampleRate: 44100,
+    bitDepth: 16
+  });
+
+  client.on('stream', function(stream, meta) {
+    console.log('new stream started for:' + client.id);
+    stream.pipe(fileWriter);
+
+    stream.on('end', function() {
+      fileWriter.end();
+      console.log('wrote to file ' + outFile);
+    });
+  });
+});
 
 
